5e4741b19f1cbeb97c4a9980b852aeab440f882c
Mehmet Kaçmaz <52501795+mehmetkacmaz@users.noreply.github.com>
2022-01-20 10:28:26 +0300

    Update gui_2.py

--- gui_2.py
+++ gui_2.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'C:/Users/kalitestajyer/Desktop/ALL/gui.ui'
-#
-# Created by: PyQt5 UI code generator 5.6
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
